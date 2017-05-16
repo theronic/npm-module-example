31575b815ba93baaf7bf08c53fa60d87126faba4
Thomas Heller <th.heller@gmail.com>
2017-05-17 00:00:34 +0200

    android also

--- examples/AwesomeProject/index.android.js
+++ examples/AwesomeProject/index.android.js
@@ -12,16 +12,13 @@ import {
   View
 } from 'react-native';
 
+import { foo } from "shadow-cljs/demo.foo";
+
 export default class AwesomeProject extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
+        {foo()}
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
